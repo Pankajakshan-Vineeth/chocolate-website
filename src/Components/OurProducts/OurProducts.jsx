1c3bbb4f4cb3db7b5b7dcda79f97ae863d70f7ca
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-05-17 17:40:41 +0530

    our products wip

--- src/Components/OurProducts/OurProducts.jsx
+++ src/Components/OurProducts/OurProducts.jsx
@@ -1,23 +1,51 @@
-import React from 'react'
-import './OurProducts.css'
+import React, { useState } from "react";
+import "./OurProducts.css";
+import Item from "../../Components/Item/Item";  
+import ourProducts from "../../assets/ourProducts";
+
+const categories = [
+  "cakes",
+  "chocolates",
+  "pastries",
+  "macarons",
+  "cookies",
+  "traditional plum cake",
+];
 
 const OurProducts = () => {
+  const [activeCategory, setActiveCategory] = useState(categories[0]); // default to "cakes"
+
   return (
-    <div className='ourproduct-container'>
-      <nav className='nav-container'>
+    <div className="ourproduct-container">
+      <nav className="nav-container">
         <h1>Our Products</h1>
 
         <ul>
-          <li>Cakes</li>
-          <li>Chocolates</li>
-          <li>Pastries</li>
-          <li>Macarons</li>
-          <li>Cookies</li>
-          <li>Traditional Plum Cake</li>
+          {categories.map((category) => (
+            <li
+              key={category}
+              className={activeCategory === category ? "active" : ""}
+              onClick={() => setActiveCategory(category)}
+              style={{ cursor: "pointer" }}
+            >
+              {category}
+            </li>
+          ))}
         </ul>
       </nav>
-    </div>
-  )
-}
 
-export default OurProducts
+      <div className="product-grid">
+        {ourProducts[activeCategory]?.map((product) => (
+          <Item
+            key={product.id}
+            name={product.name}
+            price={product.price}
+            image={product.image}
+          />
+        )) || <p>No products found in this category.</p>}
+      </div>
+    </div>
+  );
+};
+
+export default OurProducts;
