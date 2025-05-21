66319bca9484c206a83a97ce26b118d6f73a1e48
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-05-21 13:46:21 +0530

    login page ui completed

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,27 +1,43 @@
-import React from 'react'
-import './CSS/LoginSignup.css'
+import React from "react";
+import "./CSS/LoginSignup.css";
 
 const LoginSignup = () => {
   return (
-    <div className='loginsignup'>
-      <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+    <div className="login-createaccount">
+      <div className="login-container">
+        <h3>LOGIN</h3>
 
-        <div className="loginsignup-fields">
-          <input type="text"  placeholder='Your Name'/>
-          <input type="email"  placeholder='Email Address'/>
-          <input type="password"  placeholder='Password'/>
+        <div className="login-fields">
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
         </div>
 
-        <button>Continue</button>
-        <p className="loginsignup-login">Already have an account?<span>Login here</span></p>
-        <div className="loginsignup-agree">
-          <input type="checkbox" name='' id='' />
-          <p>By continuing, I agree to the terms of use and privacy policy.</p>
+        <button>Sign in</button>
+        <span className="forgot">Forgot your password?</span>
+      </div>
+
+      <div className="signup-container">
+        <h3>CREATE ACCOUNT</h3>
+
+        <div className="signup-fields">
+          <input type="text" placeholder="First name" />
+          <span>
+            <input type="text" placeholder="Last name" />
+          </span>
+
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+        </div>
+
+        <div className="signup-actions">
+          <button>Create</button>
+          <span>
+            or <a href="/">Return to Store</a>
+          </span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
